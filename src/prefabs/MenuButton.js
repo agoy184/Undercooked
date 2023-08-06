31d3f377f2b3d112a47aa054bb6e8623823809df
Samuel Zhang <sashzhan@ucsc.edu>
2023-08-06 10:49:16 -0700

    Create MenuButton.js

---
+++ src/prefabs/MenuButton.js
@@ -0,0 +1,13 @@
+class MenuButton {
+    constructor(x, y, label, scene, callback) {
+        const button = scene.add.text(x, y, label)
+            .setOrigin(0.5)
+            .setPadding(10)
+            //.setPadding(10)
+            .setStyle({ backgroundColor: '#bda280' })
+            .setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => callback())
+            .on('pointerover', () => button.setStyle({ fill: '#f39c12' }))
+            .on('pointerout', () => button.setStyle({ fill: '#FFF' }));
+    }
+}
