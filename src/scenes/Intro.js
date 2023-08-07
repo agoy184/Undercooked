ab72ca38a7ddf14dcbaee9891516a87bdb581636
Abel Goy <agoy@ucsc.edu>
2023-08-06 22:54:39 -0700

    Removed false title and bad intro text

--- src/scenes/Intro.js
+++ src/scenes/Intro.js
@@ -5,12 +5,12 @@ class Intro extends Phaser.Scene {
 
     create() {
         this.cameras.main.setBackgroundColor('rgba(0, 0, 0, 1)')
-        this.add.text(game.config.width / 2, game.config.height / 2, 'You come to visit Tokyo with your wife Tomi to visit your children Shige and Koichi, and your daughter-in-law Noriko. You two planned to also visit your youngest son Keizo in Osaka.\n\nIt\'s been over eight years since both you and Tomi last seen them.').setOrigin(0.5).setWordWrapWidth(game.config.width / 2).setAlign('center');
+        this.add.text(game.config.width / 2, game.config.height / 2, 'Day 1 of working at the worst restaraunt ever').setOrigin(0.5).setWordWrapWidth(game.config.width / 2).setAlign('center');
 
         this.cam = this.cameras.main.fadeIn(2000, 0, 0, 0);
 
         this.cam.on('camerafadeincomplete', () => {
-            this.cam = this.cameras.main.fadeOut(15000, 0, 0, 0);
+            this.cam = this.cameras.main.fadeOut(1000, 0, 0, 0);
 
             this.cam.on('camerafadeoutcomplete', () => {
                 this.scene.start('arrivalAndNeglectScene');
