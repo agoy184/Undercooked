85ad8b0d67124aad7d5966a634cabfe46cd2ec1c
Abel Goy <agoy@ucsc.edu>
2023-08-07 11:27:58 -0700

    Credits + Control fix

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -23,10 +23,13 @@ class Credits extends Phaser.Scene {
 
         // menu text
         this.add.text(game.config.width/2, game.config.height/9, 'Credits', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, 2*game.config.height/9, 'Programming: Abel Goy - Producer, Programmer, Design\n Samuel Zhang - Programmer, Design', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, 3*game.config.height/9, 'Game Design: ', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, 4*game.config.height/9, 'Sprites: ', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, 5*game.config.height/9, 'Music: ', menuConfig).setOrigin(0.5);        
+        this.add.text(game.config.width/2, 2*game.config.height/9, 'Abel Goy - Producer, Programmer, Design', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, 3*game.config.height/9, 'Samuel Zhang - Programmer, Design', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, 4*game.config.height/9, 'Alexa Medina - Character Sprites/Animations / Object Sprites / Design', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, 5*game.config.height/9, 'Daren Bartolucci - Sound Effects / Design', menuConfig).setOrigin(0.5);   
+        this.add.text(game.config.width/2, 6*game.config.height/9, 'Vixen Hayes - Music', menuConfig).setOrigin(0.5);        
+        this.add.text(game.config.width/2, 7*game.config.height/9, 'DogeSquad - Music', menuConfig).setOrigin(0.5);        
+
         this.add.text(game.config.width/2, 8*game.config.height/9, 'Press S to go back to the starting menu', menuConfig).setOrigin(0.5);
 
         keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
